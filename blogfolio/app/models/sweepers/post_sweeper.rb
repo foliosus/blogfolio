fb7319bc3803c009a55cf7e1f58f055fa0b31a46
foliosus <foliosus@ba427ba9-c427-4829-a726-0143839de3bd>
2009-03-05 03:06:18 +0000

    Fixed caching & cache expiration for category pages
    git-svn-id: svn+ssh://foliosus.com/home/foliosu/svn/blogfolio/trunk@64 ba427ba9-c427-4829-a726-0143839de3bd

--- blogfolio/app/models/sweepers/post_sweeper.rb
+++ blogfolio/app/models/sweepers/post_sweeper.rb
@@ -17,7 +17,9 @@ class PostSweeper < ActionController::Caching::Sweeper
   
   def expire_single_post(post)
     9.times{|n| expire_page(blog_path(:page => n + 1)) }
-    9.times{|n| expire_page(blog_category_path(:page => n + 1)) }
+    post.categories.each do |category|
+      9.times{|n| expire_page(blog_category_path(:category => category, :page => n + 1)) }
+    end
     expire_page(rss_path)
     expire_page(atom_path)
   end
