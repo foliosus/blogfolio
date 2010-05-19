8bb7ad412e98b863ca72ed84284e167aefd344e9
foliosu <foliosu@ba427ba9-c427-4829-a726-0143839de3bd>
2010-05-19 05:42:15 +0000

    Updated comment form to have a honeypot
    git-svn-id: svn+ssh://foliosus.com/home/foliosu/svn/blogfolio/trunk@98 ba427ba9-c427-4829-a726-0143839de3bd

--- blogfolio/app/models/modules/captcha.rb
+++ blogfolio/app/models/modules/captcha.rb
@@ -1,14 +1,21 @@
 module Captcha
-  CORRECT_ANSWER = 'why'
+  CORRECT_ANSWER = 'how'
   
   def self.included(base)
     base.class_eval do
       attr_accessor :captcha
       validate :captcha_is_correct
+      
+      attr_accessor :cap_size
+      validate :honeypot_isnt_filled
     end
     
     def captcha_is_correct
       errors.add(:captcha, "must be filled out correctly") unless self.captcha && self.captcha.downcase == CORRECT_ANSWER
+    end
+    
+    def honeypot_isnt_filled
+      errors.add_to_base("Cap size must be filled out correctly") unless self.cap_size.blank?
     end
   end
 end
